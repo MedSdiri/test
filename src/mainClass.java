a56e26026e899c37fbe8781693b2a6fbbfbb2ada
Ahmed Sdiri <sdiri.ad@gmail.com>
2020-12-02 01:24:41 -0600

    cleanUp

--- src/mainClass.java
+++ src/mainClass.java
@@ -1,10 +1,5 @@
 public class mainClass {
     public static void main(String[] args) {
-        System.out.println("chilling");
-        System.out.println("Still chilling");
-        System.out.println("first print in the first branch");
-        System.out.println("second code in the first branch");
-        System.out.println("third code in the first branch");
-        System.out.println("just messing with the firstBranch");
+
     }
 }
---
+++ src/method1.java
@@ -0,0 +1,8 @@
+public class method1 {
+    public static void main(String[] args) {
+        method1();
+    }
+    public static  void method1(){
+        System.out.println("first method");
+    }
+}
