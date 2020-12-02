ae010ada03b7ff02a8e280c499e7d642a678324e
Ahmed Sdiri <sdiri.ad@gmail.com>
2020-12-02 00:54:39 -0600

    second code in the first branch inside the first branch

--- src/mainClass.java
+++ src/mainClass.java
@@ -6,5 +6,6 @@ public class mainClass {
         System.out.println("second code in the first branch");
         System.out.println("third code in the first branch");
         System.out.println("first print in the first branch of the first branch");
+        System.out.println("second print in the first branch inside first branch");
     }
 }
