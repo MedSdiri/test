6e8d0c4aa8499e331aa0b9ba4997388530fe4cd5
Ahmed Sdiri <sdiri.ad@gmail.com>
2020-12-02 00:49:45 -0600

    second print in the first branch

--- src/mainClass.java
+++ src/mainClass.java
@@ -3,5 +3,6 @@ public class mainClass {
         System.out.println("chilling");
         System.out.println("Still chilling");
         System.out.println("first print in the first branch");
+        System.out.println("second code in the first branch");
     }
 }
