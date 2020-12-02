10ac0dfba055057e4d9ec53de20ac3221506b8f4
Ahmed Sdiri <sdiri.ad@gmail.com>
2020-12-02 00:48:40 -0600

    created a branch and added a like sout in the new branch

--- src/mainClass.java
+++ src/mainClass.java
@@ -2,5 +2,6 @@ public class mainClass {
     public static void main(String[] args) {
         System.out.println("chilling");
         System.out.println("Still chilling");
+        System.out.println("first print in the first branch");
     }
 }
