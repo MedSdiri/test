b7405bb9b75bd8e3929c361c3187cfbe12a411b1
Ahmed Sdiri <sdiri.ad@gmail.com>
2020-12-02 01:04:35 -0600

    almost done

--- src/mainClass.java
+++ src/mainClass.java
@@ -8,5 +8,6 @@ public class mainClass {
         System.out.println("just messing with the firstBranch");
         System.out.println("branch inside two branches");
         System.out.println("One more thing almost done");
+        System.out.println("branchInside2Branches");
     }
 }
